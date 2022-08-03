ce238762667762679505df9299a63fb1466ac5cc
Grayfox <alfredjr.gfx@gmail.com>
2022-08-03 21:54:42 +0800

    fizzbuzz solved

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,18 @@
+"""
+Print 1 - 100
+Print fizz if number is divisible by 3
+Print buzz if number is divisble by 5
+Print fizzbuzz if number is divisible by both 3 and 5
+"""
 num = 1
 
 while num <= 100:
-    print(num)
+    if num % 3 == 0 and num % 5 == 0:
+        print("fizzbuzz")
+    elif num % 3 == 0:
+        print("fizz")
+    elif num % 5 == 0:
+        print("buzz")
+    else:
+        print(num)
     num += 1
