d7561529244308693be6abf23c7bc004bc3b57d9
Grayfox <alfredjr.gfx@gmail.com>
2022-08-05 20:06:36 +0800

    added lives

--- Guess_the_number_game.py
+++ Guess_the_number_game.py
@@ -1,10 +1,11 @@
 from random import *
-from colorama import Fore, Back, Style, init
+from colorama import Fore, init
 
 init(autoreset=True)
 
 max_range = randint(5, 100)
 guess = None
+lives = 6
 
 print("****************************************")
 print(f"Guess the number from 0 and {max_range}")
@@ -14,6 +15,7 @@ num_to_guess = randint(0, max_range)
 
 while guess != num_to_guess:
     try:
+        print(f"Guess credit ({lives})")
         guess = int(input("What is your guess? \n"))
         last_guess = guess
 
@@ -43,7 +45,11 @@ while guess != num_to_guess:
                     + Fore.CYAN
                     + str(last_guess)
                 )
-            print("Guess again :(")
+            lives -= 1
+            if lives == 0:
+                print(f"\nRan out of guess credit ({lives})")
+                print("GAME OVER")
+                break
     except:
         print("Please enter a valid number")
         continue
