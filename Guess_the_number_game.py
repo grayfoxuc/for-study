f27a9cb88594cd25bd679db1ff7bb960b7a4dc4a
Grayfox <alfredjr.gfx@gmail.com>
2022-08-05 19:41:18 +0800

    Modified the game to include colors in terminal and try except

--- Guess_the_number_game.py
+++ Guess_the_number_game.py
@@ -1,9 +1,10 @@
-from ast import Not
-from operator import is_not
 from random import *
+from colorama import Fore, Back, Style, init
 
-max_range = randint(5,100)
-guess=None
+init(autoreset=True)
+
+max_range = randint(5, 100)
+guess = None
 
 print("****************************************")
 print(f"Guess the number from 0 and {max_range}")
@@ -12,16 +13,37 @@ num_to_guess = randint(0, max_range)
 
 
 while guess != num_to_guess:
-    guess = int(input("What is your guess? \n"))
-    last_guess = guess
-    if guess == num_to_guess:
-        print(f"Lucky guess! the number is {num_to_guess}")
-    else:
-        print("****************************************")
-        print(f"Guess the number from 0 and {max_range}")
-        print("****************************************")
-        if guess < num_to_guess:
-            print(f"Clue: **HIGHER! --your last guess is {last_guess}")
+    try:
+        guess = int(input("What is your guess? \n"))
+        last_guess = guess
+
+        if guess == num_to_guess:
+            print(Fore.YELLOW + f"Lucky guess! the number is {num_to_guess}")
         else:
-            print(f"Clue: **LOWER! --your last guess is {last_guess}")
-        print("Guess again :(")
+            print("****************************************")
+            print(f"Guess the number from 0 and {max_range}")
+            print("****************************************")
+            if guess < num_to_guess:
+                print(
+                    f"Clue: "
+                    + Fore.RED
+                    + "**HIGHER!"
+                    + Fore.WHITE
+                    + " --your last guess is "
+                    + Fore.CYAN
+                    + str(last_guess)
+                )
+            else:
+                print(
+                    "Clue: "
+                    + Fore.RED
+                    + "**LOWER!"
+                    + Fore.WHITE
+                    + " --your last guess is "
+                    + Fore.CYAN
+                    + str(last_guess)
+                )
+            print("Guess again :(")
+    except:
+        print("Please enter a valid number")
+        continue
