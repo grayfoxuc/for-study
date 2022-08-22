561c05c06d6c5f136fb72d5ffcc303761f690b73
grayfoxuc <alfredjr.uc@gmail.com>
2022-08-22 15:19:27 +0800

    changed console colors

--- Guess_the_number_game.py
+++ Guess_the_number_game.py
@@ -1,54 +1,43 @@
 from random import *
-from colorama import Fore, init
+from rich.console import Console as c
+import os
 
-init(autoreset=True)
+clear = lambda: os.system('cls')
+
+c = c()
+
+clear()
 
 max_range = randint(5, 100)
 guess = None
 lives = 6
 
-print("****************************************")
-print(f"Guess the number from 0 and {max_range}")
-print("****************************************")
+c.print("\n[yellow]****************************************[/]")
+c.print("Guess the number from 0 and "+ str(max_range), style="bold")
+c.print("[yellow]****************************************[/]")
 num_to_guess = randint(0, max_range)
 
 
 while guess != num_to_guess:
     try:
-        print(f"Guess credit ({lives})")
-        guess = int(input("What is your guess? \n"))
+        c.print(f"Guess credit ({lives})")
+        guess = int(input("\nWhat is your guess? \n"))
         last_guess = guess
 
         if guess == num_to_guess:
-            print(Fore.YELLOW + f"Lucky guess! the number is {num_to_guess}")
+            print(f"Lucky guess! the number is {num_to_guess}")
         else:
-            print("****************************************")
-            print(f"Guess the number from 0 and {max_range}")
-            print("****************************************")
+            c.print("\n[yellow]****************************************[/]")
+            c.print(f"Guess the number from 0 and {max_range}", style="bold")
+            c.print("[yellow]****************************************[/]")
             if guess < num_to_guess:
-                print(
-                    f"Clue: "
-                    + Fore.RED
-                    + "**HIGHER!"
-                    + Fore.WHITE
-                    + " --your last guess is "
-                    + Fore.CYAN
-                    + str(last_guess)
-                )
+                c.print(f"Clue: **HIGHER! your last guess is {str(last_guess)}")
             else:
-                print(
-                    "Clue: "
-                    + Fore.RED
-                    + "**LOWER!"
-                    + Fore.WHITE
-                    + " --your last guess is "
-                    + Fore.CYAN
-                    + str(last_guess)
-                )
+                c.print(f"Clue: **LOWER! your last guess is {str(last_guess)}")
             lives -= 1
             if lives == 0:
-                print(f"\nRan out of guess credit ({lives})")
-                print(f"GAME OVER: the correct number is {num_to_guess}")
+                c.print(f"\nRan out of guess credit ({lives})")
+                c.print(f"GAME OVER: the correct number is {num_to_guess}")
                 break
     except:
         print("Please enter a valid number")
